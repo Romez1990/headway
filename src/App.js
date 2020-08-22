b98a887f09659f2ee5d56d8c20950a37ee73a8df
Romez <Romez1990@gmail.com>
2020-08-22 18:22:21 +0300

    feat: setup a place for the layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 /*global chrome*/
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {this.props.isExt ? 
-            <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
-          :
-            <img src={logo} className="App-logo" alt="logo" />
-          }
-
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="app">
+            a place to place
+        </div>
     );
-  }
 }
 
 export default App;
